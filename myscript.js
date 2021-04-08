55550923a807f82215ac0651b619bfd7996cdfd1
Sergio <sergio.marretta@libero.it>
2021-04-08 17:32:41 +0200

    bonus

--- myscript.js
+++ myscript.js
@@ -8,6 +8,10 @@ mounted: function(){
 axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((result) => {
 
 this.albums = result.data.response;
+this.albums.sort(function(a,b){
+  return a.year - b.year;
+}
+);
 console.log(this.albums);
 
 });
