6e33339392470bce2431f9375e84308d93749895
Sergio <sergio.marretta@libero.it>
2021-04-08 16:32:06 +0200

    css fix 1

--- myscript.js
+++ myscript.js
@@ -1,1 +1,23 @@
+var app = new Vue(
+{
+el: "#app",
+data: {
+  albums: []
+},
+mounted: function(){
+axios.get("https://flynn.boolean.careers/exercises/api/array/music").then(function(response){
+console.log(response.data);
 
+
+});
+
+
+}
+
+
+
+
+
+
+}
+);
