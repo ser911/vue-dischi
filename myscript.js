c1821f0ca012f50e8f5230389d4e630ffa067dc3
Sergio <sergio.marretta@libero.it>
2021-04-08 18:20:00 +0200

    array  bonus 1

--- myscript.js
+++ myscript.js
@@ -2,18 +2,25 @@ var app = new Vue(
 {
 el: "#app",
 data: {
-  albums: []
+  albums: [],
+  categories:[],
 },
 mounted: function(){
 axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((result) => {
 
 this.albums = result.data.response;
+
 this.albums.sort(function(a,b){
   return a.year - b.year;
 }
 );
+for (var x in this.albums) {
+  if (this.categories.includes(this.albums[x].genre) == false) {
+    this.categories.push(this.albums[x].genre)
+  }
+}
 console.log(this.albums);
-
+console.log(this.categories);
 });
 
 }
